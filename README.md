72b22f3c8a4bee23c60d92ebf89d9a32131bb7a4
Pablo Betancor <pablojose.betancor@autentia.com>
2019-01-17 10:16:31 +0100

    Autodocumentación con swagger

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 Típica aplicación de notas con tareas TODO para poder usar de prueba en diferentes ejemplos.
 
+Operaciones documentadas en swagger: http://localhost:8080/todolist/swagger-ui.htm
--- src/main/java/com/autentia/todolist/controllers/TodoController.java
+++ src/main/java/com/autentia/todolist/controllers/TodoController.java
@@ -2,12 +2,14 @@ package com.autentia.todolist.controllers;
 
 import com.autentia.todolist.model.Todo;
 import com.autentia.todolist.services.TodoService;
+import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(description = "Operaciones para visualizar, crear y modificar notas.")
 @RestController
 public class TodoController {
 
@@ -17,8 +19,12 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @ApiOperation("Devuelve una lista con las notas")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Listado de notas devueltas correctamente")
+    })
     @GetMapping("/todos")
-    public List<Todo> list(@RequestParam boolean showOnlyPendings) {
+    public List<Todo> list(@ApiParam("Indica si sólo se quieren mostrar las notas no marcadas como finalizadas") @RequestParam boolean showOnlyPendings) {
         return todoService.list(showOnlyPendings);
     }
 }
